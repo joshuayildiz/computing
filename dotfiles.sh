ab526b21c177a50ab50227f7fff3fcb8f3cf0be6
joshuayildiz <joshuayildiz@icloud.com>
2026-03-02 11:30:10 +0300

    Now using vscode :D

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,16 +5,5 @@ set -e
 ln -sf "$(pwd)/zshrc"     "$HOME/.zshrc"
 ln -sf "$(pwd)/gitconfig" "$HOME/.gitconfig"
 
-# plugins for sublime text
-subl_plugin() {
-	path="$HOME/Library/Application Support/Sublime Text/Packages/User/$2"
-	cp "$(pwd)/sublime-text/$1" "$path"
-	echo "subl_plugin: $1"
-}
-
-subl_plugin settings.json Preferences.sublime-settings
-subl_plugin commands.json Default.sublime-commands
-subl_plugin align.py      align.py
-
 # packages
-rm Brewfile && brew bundle dump
+rm -f Brewfile && brew bundle dump
